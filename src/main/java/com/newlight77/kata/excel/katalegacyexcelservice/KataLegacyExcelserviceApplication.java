cb3053e9e55899ecffd5d051abd42f7fe1cff059
Kong To <newlight77@gmail.com>
2019-02-08 10:44:21 +0100

    initial commit with springboot project

---
+++ src/main/java/com/newlight77/kata/excel/katalegacyexcelservice/KataLegacyExcelserviceApplication.java
@@ -0,0 +1,14 @@
+package com.newlight77.kata.excel.katalegacyexcelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataLegacyExcelserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataLegacyExcelserviceApplication.class, args);
+	}
+
+}
+
